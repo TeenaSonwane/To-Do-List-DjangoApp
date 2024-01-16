54e48e1ca840a2ca09ca137c26c0b88ba8a49081
TeenaSonwane <155054727+TeenaSonwane@users.noreply.github.com>
2024-01-16 22:46:07 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5_=%%4@a6fs(be0zpfd_zany)!=l(xls575#!c4^q^w#20e-f-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
